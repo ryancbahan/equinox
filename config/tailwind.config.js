4a9f94a2005a10ee357ee75eee3e4591ff221816
Ryan  Bahan <ryan@ryanbahan.com>
2024-09-29 14:39:23 -0600

    declarative theming

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,47 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        tertiary: 'var(--color-tertiary)',
+        success: 'var(--color-success)',
+        warning: 'var(--color-warning)',
+        critical: 'var(--color-critical)',
+        info: 'var(--color-info)',
+        neutral: {
+          light: 'var(--color-neutral-light)',
+          DEFAULT: 'var(--color-neutral-default)',
+          dark: 'var(--color-neutral-dark)',
+        },
+        background: 'var(--color-background)',
+        surface: 'var(--color-surface)',
+      },
+      textColor: {
+        primary: 'var(--text-primary)',
+        secondary: 'var(--text-secondary)',
+        tertiary: 'var(--text-tertiary)',
+        success: 'var(--text-success)',
+        warning: 'var(--text-warning)',
+        critical: 'var(--text-critical)',
+        info: 'var(--text-info)',
+        neutral: {
+          light: 'var(--text-neutral-light)',
+          DEFAULT: 'var(--text-neutral-default)',
+          dark: 'var(--text-neutral-dark)',
+        },
+      },
+      borderColor: {
+        success: 'var(--color-success)',
+        warning: 'var(--color-warning)',
+        critical: 'var(--color-critical)',
+        info: 'var(--color-info)',
+        neutral: {
+          DEFAULT: 'var(--color-neutral-default)',
+          light: 'var(--color-neutral-light)',
+          dark: 'var(--color-neutral-dark)',
+        },
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
