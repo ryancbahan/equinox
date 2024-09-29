88790e0614cf0881f2ef1d0ddb9d3c5fd5516b74
Ryan  Bahan <ryan@ryanbahan.com>
2024-09-29 11:31:18 -0600

    add collapsible

---
+++ app/javascript/controllers/collapsible_controller.js
@@ -0,0 +1,11 @@
+// app/javascript/controllers/collapsible_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = ["content"]
+
+    toggle(event) {
+        event.preventDefault()
+        this.contentTarget.classList.toggle("hidden")
+    }
+}
